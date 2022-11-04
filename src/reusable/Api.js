36bda0b8faec6845d750ce66d3fdf3f28d34e59e
gitUser <gituser@gmail.com>
2022-11-04 15:09:29 +0000

    autocommit

--- src/reusable/Api.js
+++ src/reusable/Api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const apiService = async (method, url, data) => {
     const result = await axios({
         method: method,
-        url: "http://localhost:8081" + url,
+        url: "http://localhost:3035" + url,
         data: data
     }).then(res => {
         return res
--- src/views/viewAppointment/ViewAppointment.js
+++ src/views/viewAppointment/ViewAppointment.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import TheContent from "src/containers";
+import TheHeader from "src/containers";
+import TheLayout from "src/containers";
+import TheSidebar from "src/containers";
+import TheFooter from "src/containers";
 
 const ViewAppointment = () => {
-  const s=axios.get(`http://localhost:8081/allfriends`)
+  const s=axios.get(`http://localhost:3035/allfriends`)
       .then(res => {
         const persons = res.data;
         this.setState({ persons });
       })
-  return s;
+  return(
+      <div>
+      <TheContent></TheContent>
+      <TheHeader />
+      <TheLayout></TheLayout>
+      <TheSidebar></TheSidebar>
+      <TheFooter></TheFooter>
+      </div>
+  );
 };
 
 export default ViewAppointment;
