799f3105ba5592d5ea626b9ff039da1b0262c4e1
gitUser <gituser@gmail.com>
2022-11-04 14:02:44 +0000

    autocommit

--- src/reusable/Api.js
+++ src/reusable/Api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const apiService = async (method, url, data) => {
     const result = await axios({
         method: method,
-        url: "http://localhost:3500" + url,
+        url: "http://localhost:8081" + url,
         data: data
     }).then(res => {
         return res
